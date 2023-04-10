135c4896066fc63143d4b015140ec3f876019281
rifnur84 <121766288+rifnur84@users.noreply.github.com>
2023-04-10 17:42:17 +0300

    Revert "Добавлен Kafka"

--- tasktracker-kafka/src/main/kotlin/ru.rifnur.tasktracker.app.kafka/main.kt
+++
@@ -1,7 +0,0 @@
-package ru.rifnur.tasktracker.app.kafka
-
-fun main() {
-    val config = AppKafkaConfig()
-    val consumer = AppKafkaConsumer(config, listOf(ConsumerStrategyV2()))
-    consumer.run()
-}
